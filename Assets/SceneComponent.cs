ef18ea4bf97b653a59abf0fd4253d895441c22b1
el <el@el.com>
2024-04-26 19:05:20 +0700

    tempFIx

---
+++ Assets/SceneComponent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static LevelChangeObserver;
+
+public class SceneComponent : MonoBehaviour
+{
+    [SerializeField] public allScenes IdScene;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

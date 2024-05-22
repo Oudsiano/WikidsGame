6a15344817c448d969c3406a638d86500dc1745f
Marat <mar.davidoff@gmail.com>
2024-05-22 11:38:46 +0300

    EndScene

---
+++ Assets/Game/Resources/Comics/End/GameExit.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameExit : MonoBehaviour
+{
+    // Этот метод можно привязать к кнопке выхода
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        // Для остановки игры в редакторе Unity
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // Для завершения игры при сборке
+        Application.Quit();
+#endif
+    }
+}

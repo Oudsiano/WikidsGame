410fa76d493260e24ea873f0cf5fd21afcbb0258
Marat <mar.davidoff@gmail.com>
2024-06-04 23:52:52 +0300

    Fix

--- Assets/Game/Scripts/Core/Quests/QuestManager.cs
+++ Assets/Game/Scripts/Core/Quests/QuestManager.cs
@@ -9,23 +9,21 @@ public enum QuestType
 {
     killEnemy,
     toSpeekNPC,
-
 }
 
 public class QuestManager : MonoBehaviour
 {
     public List<OneQuest> thisQuestsScene;
-
     public List<ItemDefinition> allQuestsItems;
     private List<UiOneQuestElement> QuestsInScene;
 
     private QuestsForThisScene _QuestsForThisScene;
 
-
     public static event Action KillEnemy;
 
     public void Init()
     {
+        QuestsInScene = new List<UiOneQuestElement>();
         SceneManager.sceneLoaded += SceneLoader_LevelChanged;
     }
 
@@ -34,7 +32,8 @@ public class QuestManager : MonoBehaviour
         QuestsInScene = new List<UiOneQuestElement>();
         _QuestsForThisScene = FindObjectOfType<QuestsForThisScene>();
         thisQuestsScene = new List<OneQuest>();
-        if (_QuestsForThisScene!=null)
+
+        if (_QuestsForThisScene != null)
         {
             thisQuestsScene = new List<OneQuest>(_QuestsForThisScene.QuestsThisScene);
         }
@@ -43,37 +42,79 @@ public class QuestManager : MonoBehaviour
         {
             foreach (Transform child in IGame.Instance.UIManager.QuestsContentScrollRect.content)
             {
-                if (child.name!= "EmptySpace")
-                Destroy(child.gameObject);
+                if (child.name != "EmptySpace")
+                {
+                    Destroy(child.gameObject);
+                }
             }
 
-            foreach(OneQuest quest in thisQuestsScene)
+            foreach (OneQuest quest in thisQuestsScene)
             {
                 GameObject newQuest = Instantiate(IGame.Instance.UIManager.OneQuestPref, IGame.Instance.UIManager.QuestsContentScrollRect.content);
 
-                newQuest.GetComponent<UiOneQuestElement>().setQuest(quest);
-                QuestsInScene.Add(newQuest.GetComponent<UiOneQuestElement>());
+                var questElement = newQuest.GetComponent<UiOneQuestElement>();
+                if (questElement != null)
+                {
+                    questElement.setQuest(quest);
+                    QuestsInScene.Add(questElement);
+                }
+                else
+                {
+                    Debug.LogError("UiOneQuestElement component is missing on newQuest object");
+                }
             }
         }
-
     }
 
     public void newKill()
     {
+        if (QuestsInScene == null)
+        {
+            Debug.LogError("QuestsInScene is null in newKill");
+            return;
+        }
+
         foreach (UiOneQuestElement item in QuestsInScene)
         {
+            if (item == null)
+            {
+                Debug.LogError("UiOneQuestElement item is null in QuestsInScene");
+                continue;
+            }
+
             if (item.QuestType == QuestType.killEnemy)
+            {
                 item.addOneProcess();
+            }
         }
     }
 
     public void startedConversation(ConversationStarter conversationStarter)
     {
+        if (conversationStarter == null)
+        {
+            Debug.LogError("conversationStarter is null in startedConversation");
+            return;
+        }
+
+        if (QuestsInScene == null)
+        {
+            Debug.LogError("QuestsInScene is null in startedConversation");
+            return;
+        }
+
         foreach (UiOneQuestElement item in QuestsInScene)
         {
-            if (item.QuestType == QuestType.toSpeekNPC)
+            if (item == null)
+            {
+                Debug.LogError("UiOneQuestElement item is null in QuestsInScene");
+                continue;
+            }
 
+            if (item.QuestType == QuestType.toSpeekNPC)
+            {
                 item.startedConversation(conversationStarter);
+            }
         }
     }
 }

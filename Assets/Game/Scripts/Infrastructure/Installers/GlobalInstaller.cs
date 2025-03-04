e4e1b707cacb88f61d0b66b56f2f8ba97a12de21
Alexander Eroshchenko <diablo9818@gmail.com>
2025-03-04 19:03:42 +0600

    Work in progress with UI(add UI for questions)

--- Assets/Game/Scripts/Infrastructure/Installers/GlobalInstaller.cs
+++ Assets/Game/Scripts/Infrastructure/Installers/GlobalInstaller.cs
@@ -73,8 +73,8 @@ namespace Infrastructure.Installers
             Container.Bind<QuestManager>().FromComponentInNewPrefab(_questManagerPrefab).AsSingle().NonLazy();
             Container.Bind<NPCManagment>().FromComponentInNewPrefab(_npcManagerPrefab).AsSingle().NonLazy();
             Container.Bind<CursorManager>().FromComponentInNewPrefab(_cursorManagerPrefab).AsSingle().NonLazy();
-            // Container.Bind<UIManager>().FromComponentInNewPrefab(_uiManagerPrefab).AsSingle().NonLazy();
-            Container.Bind<UIManager>().FromComponentInNewPrefab(_uiManagerMobilePrefab).AsSingle().NonLazy(); //TO DO озможно нужно будет в этом месте делать проверку на девайс
+            Container.Bind<UIManager>().FromComponentInNewPrefab(_uiManagerPrefab).AsSingle().NonLazy();
+            // Container.Bind<UIManager>().FromComponentInNewPrefab(_uiManagerMobilePrefab).AsSingle().NonLazy(); //TO DO озможно нужно будет в этом месте делать проверку на девайс
             Container.Bind<CoinManager>().FromComponentInNewPrefab(_coinManagerPrefab).AsSingle().NonLazy();
             Container.Bind<WeaponArmorManager>().FromComponentInNewPrefab(_weaponArmorManagerPrefab).AsSingle()
                 .NonLazy();

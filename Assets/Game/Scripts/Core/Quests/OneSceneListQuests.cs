01d716459a0bec0d997aafe32c8379730fafbad8
Nikita Marchenko <jeirei7@icloud.com>
2025-03-07 08:36:01 +0300

    Changed Logic options to choose scenes in inspector

--- Assets/Game/Scripts/Core/Quests/OneSceneListQuests.cs
+++ Assets/Game/Scripts/Core/Quests/OneSceneListQuests.cs
@@ -4,13 +4,35 @@ using NaughtyAttributes;
 using SceneManagement;
 using SceneManagement.Enums;
 using UnityEngine;
+using Utils;
 
 namespace Core.Quests
 {
     [System.Serializable]
     public class OneSceneListQuests
     {
-        [SerializeField] public string SceneId; // TODO using error
+        [Dropdown("GetSceneNames")][SerializeField] public string SceneId; // TODO using error
         [SerializeField] public List<OneQuest> QuestsThisScene; // TODO O/C error
+        
+        private List<string> GetSceneNames()
+        {
+            return new List<string>
+            {
+                Constants.Scenes.BootstrapScene,
+                Constants.Scenes.OpenScene,
+                Constants.Scenes.MapScene,
+                Constants.Scenes.FirstTownScene,
+                Constants.Scenes.FirstBattleScene,
+                Constants.Scenes.SecondBattleScene,
+                Constants.Scenes.ThirdBattleScene,
+                Constants.Scenes.FourthBattleSceneDark,
+                Constants.Scenes.FifthBattleSceneKingdom,
+                Constants.Scenes.SixthBattleSceneKingdom,
+                Constants.Scenes.SeventhBattleSceneViking,
+                Constants.Scenes.LibraryScene,
+                Constants.Scenes.HollScene,
+                Constants.Scenes.EndScene
+            };
+        }
     }
 }
--- Assets/Game/Scripts/SceneManagement/OneBtnChangeRegion.cs
+++ Assets/Game/Scripts/SceneManagement/OneBtnChangeRegion.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using NaughtyAttributes;
 using SceneManagement.Enums;
 using UnityEngine;
 using UnityEngine.UI;
+using Utils;
 
 namespace SceneManagement
 {
@@ -10,7 +12,7 @@ namespace SceneManagement
     public class OneBtnChangeRegion // TODO rename
     {
         public Button Button;
-        public string loadedScene;
+        [Dropdown("GetSceneNames")] public string loadedScene;
 
         public void SetRed()
         {
@@ -25,6 +27,27 @@ namespace SceneManagement
         internal void SetNormal()
         {
             Button.GetComponent<Image>().color = new Color32(0xff, 0xFF, 0xfF, 0xFF); // TODO can be cached
+        }
+
+        private List<string> GetSceneNames()
+        {
+            return new List<string>
+            {
+                Constants.Scenes.BootstrapScene,
+                Constants.Scenes.OpenScene,
+                Constants.Scenes.MapScene,
+                Constants.Scenes.FirstTownScene,
+                Constants.Scenes.FirstBattleScene,
+                Constants.Scenes.SecondBattleScene,
+                Constants.Scenes.ThirdBattleScene,
+                Constants.Scenes.FourthBattleSceneDark,
+                Constants.Scenes.FifthBattleSceneKingdom,
+                Constants.Scenes.SixthBattleSceneKingdom,
+                Constants.Scenes.SeventhBattleSceneViking,
+                Constants.Scenes.LibraryScene,
+                Constants.Scenes.HollScene,
+                Constants.Scenes.EndScene
+            };
         }
     }
 }

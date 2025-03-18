19d18f0c183f1164d9081745651a6a6beedc0655
Nikita Marchenko <jeirei7@icloud.com>
2025-03-18 09:17:22 +0300

    Add UIManager image
    Deactivate MapSetter

--- Assets/Game/Scripts/MapSetter.cs
+++ Assets/Game/Scripts/MapSetter.cs
@@ -5,43 +5,43 @@ using UnityEngine.Serialization;
 
 public class ImageSetter : MonoBehaviour
 {
-    [FormerlySerializedAs("imageComponent")] [SerializeField]
-    private RawImage _imageComponent; // Ссылка на компонент Image в вашем объекте
-
-    //public Texture2D[] imageArray; // Массив изображений, которые вы хотите использовать
-    [FormerlySerializedAs("renderTexturesArray")] [SerializeField]
-    private RenderTexture _renderTexturesArray;
-
-    private void Start() 
-    {
-        if (_imageComponent == null)
-        {
-            _imageComponent = gameObject.GetComponent<RawImage>();
-
-            if (_imageComponent == null)
-            {
-                Debug.LogError("Image component is not assigned!");
-
-                return;
-            }
-        }
-
-        // Добавляем метод UpdateImage как слушатель события перехода на новую сцену
-        SceneManager.sceneLoaded += UpdateImage;
-
-        // Вызываем UpdateImage для установки изображения на текущей сцене
-        UpdateImage(SceneManager.GetActiveScene(), LoadSceneMode.Single);
-    }
-
-    private void OnDestroy()
-    {
-        SceneManager.sceneLoaded -= UpdateImage;
-    }
-
-    private void UpdateImage(Scene scene, LoadSceneMode mode)
-    {
-        // Проверяем, существует ли изображение для текущей сцены // TODO idk
-        // Устанавливаем изображение из массива, соответствующее номеру сцены
-        _imageComponent.texture = _renderTexturesArray;
-    }
+    // [FormerlySerializedAs("imageComponent")] [SerializeField]
+    // private RawImage _imageComponent; // Ссылка на компонент Image в вашем объекте
+    //
+    // //public Texture2D[] imageArray; // Массив изображений, которые вы хотите использовать
+    // [FormerlySerializedAs("renderTexturesArray")] [SerializeField]
+    // private RenderTexture _renderTexturesArray;
+    //
+    // private void Start() 
+    // {
+    //     if (_imageComponent == null)
+    //     {
+    //         _imageComponent = gameObject.GetComponent<RawImage>();
+    //
+    //         if (_imageComponent == null)
+    //         {
+    //             Debug.LogError("Image component is not assigned!");
+    //
+    //             return;
+    //         }
+    //     }
+    //
+    //     // Добавляем метод UpdateImage как слушатель события перехода на новую сцену
+    //     SceneManager.sceneLoaded += UpdateImage;
+    //
+    //     // Вызываем UpdateImage для установки изображения на текущей сцене
+    //     UpdateImage(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+    // }
+    //
+    // private void OnDestroy()
+    // {
+    //     SceneManager.sceneLoaded -= UpdateImage;
+    // }
+    //
+    // private void UpdateImage(Scene scene, LoadSceneMode mode)
+    // {
+    //     // Проверяем, существует ли изображение для текущей сцены // TODO idk
+    //     // Устанавливаем изображение из массива, соответствующее номеру сцены
+    //     _imageComponent.texture = _renderTexturesArray;
+    // }
 }
